1987b382fe8b52191ef1e260553d9a0f7f205bbc
ElengPregunts28 <ellainepregunta28@gmail.com>
2024-12-17 13:23:27 +0800

    Improving styles.

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router'
 import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth"
 import { useNavigate } from "react-router"
 import { googleProvider, authentication } from "../Firebase"
+import GoogleIcon from './GoogleIcon'
+import { FaGoogle } from "react-icons/fa";
 
 export const SignIn = () => {
     const [email, setEmail] = useState('')
@@ -25,21 +27,21 @@ export const SignIn = () => {
             await signInWithPopup(authentication, googleProvider)
             navigate('/')
         } catch (error) {
-            setError(`Error: ${error}`)
+            setError(`${error}`)
         }
     }
 
 
     return (
-      <div>
+      <div className="box">
         <h1>Sign In</h1>
-        <input type="email" placeholder="Enter your email..." onChange={(e) => setEmail(e.target.value)}/> <br />
-        <input type="password" placeholder="Enter your password..." onChange={(e) => setPassword(e.target.value)}/> <br />
-        <button onClick={handleSignIn}>Sign In</button> <br />
+        <input type="email" placeholder="Enter your email..." className="user-input" onChange={(e) => setEmail(e.target.value)}/> <br />
+        <input type="password" placeholder="Enter your password..." className="user-input" onChange={(e) => setPassword(e.target.value)}/> <br />
+        <button onClick={handleSignIn} className="sign-in">Sign In</button> <br />
         { 'or' } <br />
-        <button onClick={handleSignInWithGmail}>Sign In with your Gmail</button>
-        {error && <p>{error}</p>}
-        <p>Don't have account yet? <Link to = "/SignUp">SignUp</Link></p>
+        <button onClick={handleSignInWithGmail} className="sign-in-google">Sign In with your <FaGoogle /></button>
+        {error && <p>{error}</p> }
+        <p>Don't have account yet? <Link to = "/SignUp" className="link">Sign Up</Link></p>
       </div>
     )
 }
--- src/Components/SignOut.jsx
+++ src/Components/SignOut.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { useNavigate } from "react-router";
 import { signOut } from "firebase/auth";
 import { authentication } from "../Firebase";
+import { ImExit } from "react-icons/im";
 
 export const SignOut = () => {
     const navigate = useNavigate()
 
     const HandleSignOut = async () => {
         await signOut(authentication)
-        navigate('/signin')
+        navigate('/SignIn')
     }
 
-    return <button onClick={HandleSignOut}>Sign Out</button>
+    return <button onClick={HandleSignOut} className="exit"> <ImExit /> Exit</button>
 }
